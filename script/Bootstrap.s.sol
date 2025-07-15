f24a6fadc0f5788af8786e01b5aeed2e180dfdf9
cryptonoyaiba <71776468+cryptonoyaiba@users.noreply.github.com>
2025-07-15 19:46:27 +0100

    Deployed newer Bootstrap contracts
    - Deployed and verified `Bootstrap` on supported chains.
    - Updated Bootstrap.s.sol to check for existing counterfactual deployment.
    - Added deployment information.

--- script/Bootstrap.s.sol
+++ script/Bootstrap.s.sol
@@ -11,6 +11,9 @@ import {Bootstrap} from "../src/utils/Bootstrap.sol";
  * @dev Deployment script for Bootstrap.
  */
 contract BootstrapScript is Script {
+    bytes32 public immutable SALT = bytes32(abi.encodePacked("ModularEtherspotWallet:Create2:salt"));
+    address public constant EXPECTED_BOOTSTRAP = 0x8B8AD39700EB6f5903aAC9Cc34b6D28ae69D170B;
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
@@ -21,10 +24,16 @@ contract BootstrapScript is Script {
         //////////////////////////////////////////////////////////////*/
 
         console2.log("Deploying Bootstrap...");
-
-        Bootstrap bootstrap = new Bootstrap();
-
-        console2.log("Bootstrap deployed at address", address(bootstrap));
+        if (EXPECTED_BOOTSTRAP.code.length == 0) {
+            Bootstrap bootstrap = new Bootstrap{salt: SALT}();
+            if (address(bootstrap) != EXPECTED_BOOTSTRAP) {
+                revert("Unexpected bootstrap address!!!");
+            } else {
+                console2.log("Bootstrap deployed at address", address(bootstrap));
+            }
+        } else {
+            console2.log("Bootstrap already deployed at address", EXPECTED_BOOTSTRAP);
+        }
         console2.log("Finished deployment sequence!");
 
         vm.stopBroadcast();
